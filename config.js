76a6d73b596192a27c38806b5157d0f0ff9b64cd
https://gitlab.com/aisyahhhh <saisyah.zainal@gmail.com>
2024-02-15 22:09:52 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,32 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // You can write any number of sentences here, up to twenty or even more.
+    // Try to keep each sentence under 15 characters for better display.
+    texts: [
+        "To my dear",      // Each sentence must end with a comma, which is important!
+        "Beloved sweetheart",  
+        "Today is your birthday",
+        "This marks our",
+        "Third birthday together",
+        "Last year's birthday",
+        "We had a warm homemade waffles ",
+        "This year, we'll celebrate it after you coming back",
+        "And spend a lot of time with abu at home",
+        "Then cuddle up",
+        "Yummy~~",
+    ],
+    imgs: {
+        "Beloved sweetheart": "./imgs/sahira (2).jpeg",
+        // "./imgs/birthday.jpg", // This line is commented out to remove the image for "Today is your birthday".
+    },
+    desc: {
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Banner",
+        balloons_flying: "Seems like something's missing",
+        cake_fadein: "Cake?",
+        light_candle: "Candles?",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
